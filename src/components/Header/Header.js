9d8b9fb9daf180b5b95e8de50fc22471953ab77c
itc-s23023 <s23023@std.it-college.ac.jp>
2024-12-05 13:27:53 +0900

    Headerコンポーネント内でBackButtonコンポーネントを使用するように

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,10 @@
-import { useRouter } from 'next/router';
 import styles from './Header.module.css';
-import Image from 'next/image';
-import backIcon from '../../../public/image/back.svg';
+import BackButton from '../BackButton/BackButton';
 
 export default function Header({ title = 'ページタイトル' }) {
-  const router = useRouter();
-
   return (
     <header className={styles.header}>
-      <button onClick={() => router.back()} className={styles.backButton}>
-        <Image src={backIcon} alt="戻る" width={40} height={40} />
-      </button>
+      <BackButton/>
       <h1 className={styles.title}>{title}</h1>
     </header>
   );
