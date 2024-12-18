818fda9415425371c9d1da2aa08de96930c35d9d
itc-s23014 <s23014@std.it-college.ac.jp>
2024-12-18 12:23:19 +0900

    catalogページのみフックなしで

--- src/hooks/useNotifications.js
+++ src/hooks/useNotifications.js
@@ -1,36 +1,36 @@
-/**
- * 指定されたユーザーに関連する通知をリアルタイムで取得するカスタムフック。
- *
- * @returns {Array} 通知のリスト（通知がない場合は空配列）。
- */
-
-import { useEffect, useState } from 'react';
-import { collection, onSnapshot, query, where } from 'firebase/firestore';
-import { db } from '../../firebaseConfig';
-
-const useNotifications = (user) => {
-    const [notifications, setNotifications] = useState([]);
-
-    useEffect(() => {
-        if (user) {
-            const notificationsQuery = query(
-                collection(db, 'notifications'),
-                where('sellerId', '==', user.uid)
-            );
-
-            const unsubscribe = onSnapshot(notificationsQuery, (snapshot) => {
-                const notificationsList = snapshot.docs.map((doc) => ({
-                    id: doc.id,
-                    ...doc.data(),
-                }));
-                setNotifications(notificationsList);
-            });
-
-            return () => unsubscribe();
-        }
-    }, [user]);
-
-    return notifications;
-};
-
-export default useNotifications;
+// /**
+//  * 指定されたユーザーに関連する通知をリアルタイムで取得するカスタムフック。
+//  *
+//  * @returns {Array} 通知のリスト（通知がない場合は空配列）。
+//  */
+//
+// import { useEffect, useState } from 'react';
+// import { collection, onSnapshot, query, where } from 'firebase/firestore';
+// import { db } from '../../firebaseConfig';
+//
+// const useNotifications = (user) => {
+//     const [notifications, setNotifications] = useState([]);
+//
+//     useEffect(() => {
+//         if (user) {
+//             const notificationsQuery = query(
+//                 collection(db, 'notifications'),
+//                 where('sellerId', '==', user.uid)
+//             );
+//
+//             const unsubscribe = onSnapshot(notificationsQuery, (snapshot) => {
+//                 const notificationsList = snapshot.docs.map((doc) => ({
+//                     id: doc.id,
+//                     ...doc.data(),
+//                 }));
+//                 setNotifications(notificationsList);
+//             });
+//
+//             return () => unsubscribe();
+//         }
+//     }, [user]);
+//
+//     return notifications;
+// };
+//
+// export default useNotifications;
