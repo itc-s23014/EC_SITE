2146672cc7a805d6ad60d4a2720c32be11097b49
itc-s23023 <s23023@std.it-college.ac.jp>
2025-01-23 09:59:42 +0900

    検索バーコンポーネントを追加。カテゴリのsvgは動かない

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,66 @@
+import AvatarDropdown from "../AvatarDropdown";
+
+const SearchBar = () => {
+  return (
+    <div className="flex items-center gap-x-6 gap-y-4 ml-auto">
+      <div className="flex bg-gray-50 border focus-within:bg-transparent focus-within:border-gray-400 rounded-full px-4 py-2.5 overflow-hidden max-w-76 max-lg:hidden relative">
+        {/* カテゴリドロップダウンをトグルするボタン */}
+        <button aria-label="Toggle Category Dropdown" className="mr-2">
+          <svg
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24px"
+            height="24px"
+          >
+            <path
+              d="M3 6C3 4.34315 4.34315 3 6 3H7C8.65685 3 10 4.34315 10 6V7C10 8.65685 8.65685 10 7 10H6C4.34315 10 3 8.65685 3 7V6Z"
+              stroke="#000000"
+              strokeWidth="2"
+            ></path>
+            <path
+              d="M14 6C14 4.34315 15.3431 3 17 3H18C19.6569 3 21 4.34315 21 6V7C21 8.65685 19.6569 10 18 10H17C15.3431 10 14 8.65685 14 7V6Z"
+              stroke="#000000"
+              strokeWidth="2"
+            ></path>
+            <path
+              d="M14 17C14 15.3431 15.3431 14 17 14H18C19.6569 14 21 15.3431 21 17V18C21 19.6569 19.6569 21 18 21H17C15.3431 21 14 19.6569 14 18V17Z"
+              stroke="#000000"
+              strokeWidth="2"
+            ></path>
+            <path
+              d="M3 17C3 15.3431 4.34315 14 6 14H7C8.65685 14 10 15.3431 10 17V18C10 19.6569 8.65685 21 7 21H6C4.34315 21 3 19.6569 3 18V17Z"
+              stroke="#000000"
+              strokeWidth="2"
+            ></path>
+          </svg>
+        </button>
+
+        {/* 検索バー */}
+        <label htmlFor="search-input" className="sr-only">Search</label>
+        <input
+          id="search-input"
+          type="text"
+          placeholder="Search something..."
+          className="w-full text-sm bg-transparent outline-none pr-2"
+        />
+
+        {/* 検索ボタン */}
+        <button aria-label="Search" className="cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 192.904 192.904"
+            width="16px"
+            className="fill-gray-600"
+          >
+            <path
+              d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import styles from './style.module.css';
 import Carousel from '@/components/Carousel';
 import AvatarDropdown from '@/components/AvatarDropdown';
+import SearchBar from '@/components/SearchBar';
 
 const TestPage = () => {
   const [products, setProducts] = useState([]);
@@ -93,37 +94,7 @@ const TestPage = () => {
             </ul>
           </div>
 
-          <div class="flex items-center gap-x-6 gap-y-4 ml-auto">
-  <div class="flex bg-gray-50 border focus-within:bg-transparent focus-within:border-gray-400 rounded-full px-4 py-2.5 overflow-hidden max-w-76 max-lg:hidden">
-
-    <label for="category-filter" class="sr-only">Category Filter</label>
-    <select id="category-filter" class="bg-transparent text-sm outline-none pr-2  min-w-20 max-w-sm text-ellipsis overflow-hidden">
-    <option value="category" disabled selected>カテゴリ</option>
-    <option value="all">All</option>
-      <option value="new">New</option>
-      <option value="men">Men</option>
-      <option value="women">Women</option>
-      <option value="kids">Kids</option>
-      <option value="accessories">Accessoriesaaaaaaaaaaaaaaaaaaa</option>
-    </select>
-
-    <label for="search-input" class="sr-only">Search</label>
-    <input
-      id="search-input"
-      type="text"
-      placeholder="Search something..."
-      class="w-full text-sm bg-transparent outline-none pr-2"
-    />
-
-    <button aria-label="Search" class="cursor-pointer">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192.904 192.904" width="16px" class="fill-gray-600">
-        <path
-          d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z">
-        </path>
-      </svg>
-    </button>
-  </div>
-</div>
+          <SearchBar />
 
             <div class='flex items-center sm:space-x-8 space-x-6'>
               <div class="flex flex-col items-center justify-center gap-0.5 cursor-pointer">
