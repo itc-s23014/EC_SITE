4d56ab193d8f70056fb3e574253fe32a610e4c1a
itc-s23023 <s23023@std.it-college.ac.jp>
2025-01-24 12:58:02 +0900

    不要なヘッダー内のアイコンを削除

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -288,36 +288,6 @@ const TestPage = () => {
                 )}
               </div>
               <div class='flex items-center sm:space-x-8 space-x-6'>
-                <div class="flex flex-col items-center justify-center gap-0.5 cursor-pointer"
-                     onClick={() => router.push('/like-list')}>
-                  <div class="relative">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="cursor-pointer fill-[#333] inline w-5 h-5"
-                         viewBox="0 0 64 64">
-                      <path
-                          d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
-                          data-original="#000000"/>
-                    </svg>
-                    <span
-                        class="absolute left-auto -ml-1 top-0 rounded-full bg-red-500 px-1 py-0 text-xs text-white">0</span>
-                  </div>
-                  <span class="text-[13px] font-semibold text-[#333]">Wishlist</span>
-                </div>
-                <div class="flex flex-col items-center justify-center gap-0.5 cursor-pointer"
-                     onClick={() => router.push('/cart')}>
-                  <div class="relative">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px"
-                         class="cursor-pointer fill-[#333] inline"
-                         viewBox="0 0 512 512">
-                      <path
-                          d="M164.96 300.004h.024c.02 0 .04-.004.059-.004H437a15.003 15.003 0 0 0 14.422-10.879l60-210a15.003 15.003 0 0 0-2.445-13.152A15.006 15.006 0 0 0 497 60H130.367l-10.722-48.254A15.003 15.003 0 0 0 105 0H15C6.715 0 0 6.715 0 15s6.715 15 15 15h77.969c1.898 8.55 51.312 230.918 54.156 243.71C131.184 280.64 120 296.536 120 315c0 24.812 20.188 45 45 45h272c8.285 0 15-6.715 15-15s-6.715-15-15-15H165c-8.27 0-15-6.73-15-15 0-8.258 6.707-14.977 14.96-14.996zM477.114 90l-51.43 180H177.032l-40-180zM150 405c0 24.813 20.188 45 45 45s45-20.188 45-45-20.188-45-45-45-45 20.188-45 45zm45-15c8.27 0 15 6.73 15 15s-6.73 15-15 15-15-6.73-15-15 6.73-15 15-15zm167 15c0 24.813 20.188 45 45 45s45-20.188 45-45-20.188-45-45-45-45 20.188-45 45zm45-15c8.27 0 15 6.73 15 15s-6.73 15-15 15-15-6.73-15-15 6.73-15 15-15zm0 0"
-                          data-original="#000000"></path>
-                    </svg>
-                    <span
-                        class="absolute left-auto -ml-1 top-0 rounded-full bg-red-500 px-1 py-0 text-xs text-white">0</span>
-                  </div>
-                  <span class="text-[13px] font-semibold text-[#333]">Cart</span>
-                </div>
-
           <div class='flex items-center sm:space-x-8 space-x-6'>
           <div
   className="flex flex-col items-center justify-center gap-0.5 cursor-pointer"
