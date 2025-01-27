219e77cbdbb2315cb29fa4c31cd6327f2831411f
itc-s23014 <s23014@std.it-college.ac.jp>
2025-01-27 11:52:15 +0900

    少々の修正

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -320,41 +320,41 @@ const TestPage = () => {
               {/*    <span class="text-[13px] font-semibold text-[#333]">Cart</span>*/}
               {/*  </div>*/}
 
-              <div class='flex items-center sm:space-x-8 space-x-6'>
+    {/*          <div class='flex items-center sm:space-x-8 space-x-6'>*/}
 
-                <div
-                    className="flex flex-col items-center justify-center gap-0.5 cursor-pointer"
-                    onClick={() => router.push('/')}
-                >
-                  <button
-                      className="py-4 px-1 relative border-2 border-transparent text-gray-800 rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out"
-                      aria-label="Notifications">
-                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.  org/2000/svg" className="h-6 w-6">
-                      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
-                      <g id="SVGRepo_iconCarrier">
-                        <g clipPath="url(#clip0_15_159)">
-                          <rect width="24" height="24" fill="white"></rect>
-                          <path
-                              d="M9.5 19C8.89555 19 7.01237 19 5.61714 19C4.87375 19 4.39116 18.2177 4.72361 17.5528L5.57771 15.8446C5.85542 15.2892 6 14.6774 6 14.0564C6 13.2867 6 12.1434 6 11C6 9 7 5 12 5C17 5 18 9 18 11C18 12.1434 18 13.2867 18 14.0564C18 14.6774 18.1446 15.2892 18.4223 15.8446L19.2764 17.5528C19.6088 18.2177 19.1253 19 18.382 19H14.5M9.5 19C9.5 21 10.5 22 12 22C13.5 22 14.5 21 14.5 19M9.5 19C11.0621 19 14.5 19 14.5 19"
-                              stroke="#000000" strokeLinejoin="round"></path>
-                          <path d="M12 5V3" stroke="#000000" strokeLinecap="round" strokeLinejoin="round"></path>
-                        </g>
-                        <defs>
-                          <clipPath id="clip0_15_159">
-                            <rect width="24" height="24" fill="white"></rect>
-                          </clipPath>
-                        </defs>
-                      </g>
-                    </svg>
+    {/*            <div*/}
+    {/*                className="flex flex-col items-center justify-center gap-0.5 cursor-pointer"*/}
+    {/*                onClick={() => router.push('/')}*/}
+    {/*            >*/}
+    {/*              <button*/}
+    {/*                  className="py-4 px-1 relative border-2 border-transparent text-gray-800 rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out"*/}
+    {/*                  aria-label="Notifications">*/}
+    {/*                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6">*/}
+    {/*                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>*/}
+    {/*                  <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>*/}
+    {/*                  <g id="SVGRepo_iconCarrier">*/}
+    {/*                    <g clipPath="url(#clip0_15_159)">*/}
+    {/*                      <rect width="24" height="24" fill="white"></rect>*/}
+    {/*                      <path*/}
+    {/*                          d="M9.5 19C8.89555 19 7.01237 19 5.61714 19C4.87375 19 4.39116 18.2177 4.72361 17.5528L5.57771 15.8446C5.85542 15.2892 6 14.6774 6 14.0564C6 13.2867 6 12.1434 6 11C6 9 7 5 12 5C17 5 18 9 18 11C18 12.1434 18 13.2867 18 14.0564C18 14.6774 18.1446 15.2892 18.4223 15.8446L19.2764 17.5528C19.6088 18.2177 19.1253 19 18.382 19H14.5M9.5 19C9.5 21 10.5 22 12 22C13.5 22 14.5 21 14.5 19M9.5 19C11.0621 19 14.5 19 14.5 19"*/}
+    {/*                          stroke="#000000" strokeLinejoin="round"></path>*/}
+    {/*                      <path d="M12 5V3" stroke="#000000" strokeLinecap="round" strokeLinejoin="round"></path>*/}
+    {/*                    </g>*/}
+    {/*                    <defs>*/}
+    {/*                      <clipPath id="clip0_15_159">*/}
+    {/*                        <rect width="24" height="24" fill="white"></rect>*/}
+    {/*                      </clipPath>*/}
+    {/*                    </defs>*/}
+    {/*                  </g>*/}
+    {/*                </svg>*/}
     {/*                <span className="absolute inset-0 object-right-top -mr-6">*/}
     {/*  <div*/}
     {/*      className="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-red-500 text-white">*/}
     {/*    6*/}
     {/*  </div>*/}
     {/*</span>*/}
-                  </button>
-                </div>
+    {/*              </button>*/}
+    {/*            </div>*/}
 
                 <div className="flex flex-col items-center justify-center gap-0.5 cursor-pointer"
                      onClick={() => router.push('/like-list')}>
