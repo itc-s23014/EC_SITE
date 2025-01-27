e03502bbb865cd851ed0cafca9dda5847ab1df02
itc-s23014 <s23014@std.it-college.ac.jp>
2025-01-27 11:22:18 +0900

    通知のカウントとりあえず保留

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { collection, getDocs, query, where, doc, getDoc } from 'firebase/firestore';
+import {collection, getDocs, query, where, doc, getDoc, onSnapshot} from 'firebase/firestore';
 import { db } from '../../../firebaseConfig';
 import Image from 'next/image';
 import styles from './style.module.css';
@@ -30,6 +30,7 @@ const TestPage = () => {
   const toggleDropdown = () => {
     setDropdownVisible(!dropdownVisible);
   };
+
 
   const handleClickOutside = (event) => {
     if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
@@ -326,7 +327,7 @@ const TestPage = () => {
                   <button
                       className="py-4 px-1 relative border-2 border-transparent text-gray-800 rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out"
                       aria-label="Notifications">
-                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6">
+                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.  org/2000/svg" className="h-6 w-6">
                       <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                       <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
                       <g id="SVGRepo_iconCarrier">
@@ -344,12 +345,12 @@ const TestPage = () => {
                         </defs>
                       </g>
                     </svg>
-                    <span className="absolute inset-0 object-right-top -mr-6">
-      <div
-          className="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-red-500 text-white">
-        6
-      </div>
-    </span>
+    {/*                <span className="absolute inset-0 object-right-top -mr-6">*/}
+    {/*  <div*/}
+    {/*      className="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-red-500 text-white">*/}
+    {/*    6*/}
+    {/*  </div>*/}
+    {/*</span>*/}
                   </button>
                 </div>
 
@@ -366,12 +367,12 @@ const TestPage = () => {
                             d="M433.601,67.001c-24.7-24.7-57.4-38.2-92.3-38.2s-67.7,13.6-92.4,38.3l-12.9,12.9l-13.1-13.1 c-24.7-24.7-57.6-38.4-92.5-38.4c-34.8,0-67.6,13.6-92.2,38.2c-24.7,24.7-38.3,57.5-38.2,92.4c0,34.9,13.7,67.6,38.4,92.3 l187.8,187.8c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-3.9l188.2-187.5c24.7-24.7,38.3-57.5,38.3-92.4 C471.801,124.501,458.301,91.701,433.601,67.001z M414.401,232.701l-178.7,178l-178.3-178.3c-19.6-19.6-30.4-45.6-30.4-73.3 s10.7-53.7,30.3-73.2c19.5-19.5,45.5-30.3,73.1-30.3c27.7,0,53.8,10.8,73.4,30.4l22.6,22.6c5.3,5.3,13.8,5.3,19.1,0l22.4-22.4 c19.6-19.6,45.7-30.4,73.3-30.4c27.6,0,53.6,10.8,73.2,30.3c19.6,19.6,30.3,45.6,30.3,73.3 C444.801,187.101,434.001,213.101,414.401,232.701z"></path>
                       </g>
                     </svg>
-                    <span className="absolute inset-0 object-right-top -mr-6">
-      <div
-          className="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-red-500 text-white">
-        6
-      </div>
-    </span>
+    {/*                <span className="absolute inset-0 object-right-top -mr-6">*/}
+    {/*  <div*/}
+    {/*      className="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-red-500 text-white">*/}
+    {/*    6*/}
+    {/*  </div>*/}
+    {/*</span>*/}
                   </button>
                 </div>
 
@@ -385,12 +386,12 @@ const TestPage = () => {
                       <path
                           d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
                     </svg>
-                    <span class="absolute inset-0 object-right-top -mr-6">
-                  <div
-                      class="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-red-500 text-white">
-                    6
-                  </div>
-                </span>
+                {/*    <span class="absolute inset-0 object-right-top -mr-6">*/}
+                {/*  <div*/}
+                {/*      class="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-red-500 text-white">*/}
+                {/*    6*/}
+                {/*  </div>*/}
+                {/*</span>*/}
                   </button>
                 </div>
 
