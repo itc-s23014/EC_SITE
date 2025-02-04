2ff55abfc4a02ac819ef5f0dd3c1381889acd4e9
itc-s23023 <s23023@std.it-college.ac.jp>
2025-02-04 14:57:21 +0900

    Headerコンポーネントのカートアイコンがカート内の商品の個数を表示できるように

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,6 +6,7 @@ import NotificationDropdown from '@/components/NotificationDropdown';
 import { useRouter } from 'next/router';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { useSellerAndUserData } from '@/hooks/useSellerAndUserData';
+import useCart from '@/hooks/useCart';
 
 const Header = () => {
   const [products, setProducts] = useState([]);
@@ -17,6 +18,7 @@ const Header = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const { userData, sellerData, sellerName, loading: userLoading } = useSellerAndUserData(user?.uid);
   const sellerEmail = sellerData?.email;
+  const { cartItemCount } = useCart(user);
 
 
   const handleClickOutside = (event) => {
@@ -176,6 +178,13 @@ const Header = () => {
                     <path
                         d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
                   </svg>
+                  {cartItemCount > 0 && (
+            <span className="absolute inset-0 object-right-top -mr-6">
+              <div className="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-red-500 text-white">
+                {cartItemCount}
+              </div>
+            </span>
+          )}
                 </button>
               </div>
 
